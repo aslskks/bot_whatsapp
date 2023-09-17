84910f7d5ee2c6885e690f03edfb425e57d1798f
aslskks <130728002+aslskks@users.noreply.github.com>
2023-09-16 21:24:53 -0600

    code

--- bot.py
+++ bot.py
@@ -3,15 +3,13 @@ from datetime import datetime
 import time
 seconds = time.time() + 60
 
-date = datetime.fromtimestamp(seconds)
-numero_manadar = input("ingresa el numero para el mensaje: ")
-texto = input("ingresa el mensaje: ")
-print("espera 60 segundos para el mensaje")
-
-what.sendwhatmsg()
-p1 = input("quieres poner una foto s/n")
-if p1 == "s":
-  p2 = input("pon la foto en la carpeta y escribe su nombre y extencion")
-  what.sendwhat_image(numero_mandar,p2)
-else:
-  exit()
+date = datetime.fromimestamp(seconds)
+def send_message(numero, mensaje):
+  seconds = time.time() + 60
+  date = datetime.fromimestamp(seconds)
+  what.sendwhatmsg(numero, mensaje, date.hour,date.minute)
+def send_image(numero, imagen):
+  seconds = time.time() + 60
+  date = datetime.fromimestamp(seconds)
+  what.sendwhats_image(numero,imagen)
+  
