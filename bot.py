97a878e3bc20a016bd394df03f4ce39caae206fd
aslskks <130728002+aslskks@users.noreply.github.com>
2023-09-17 11:30:05 -0600

    code

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ from datetime import datetime
 import time
 seconds = time.time() + 60
 
-date = datetime.fromimestamp(seconds)
+date = datetime.fromtimestamp(seconds)
 def send_message(numero, mensaje):
   seconds = time.time() + 60
-  date = datetime.fromimestamp(seconds)
+  date = datetime.fromtimestamp(seconds)
   what.sendwhatmsg(numero, mensaje, date.hour,date.minute)
 def send_image(numero, imagen):
   seconds = time.time() + 60
