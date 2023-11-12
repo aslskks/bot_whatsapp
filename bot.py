4d415550b951d8b9b5375fc1b855d0f46449d092
aslskks <130728002+aslskks@users.noreply.github.com>
2023-11-12 11:16:24 -0600

    code

--- bot.py
+++ bot.py
@@ -4,12 +4,21 @@ import time
 seconds = time.time() + 60
 
 date = datetime.fromtimestamp(seconds)
-def send_message(numero, mensaje):
+def send_message(numero: int, mensaje: str):
   seconds = time.time() + 60
   date = datetime.fromtimestamp(seconds)
   what.sendwhatmsg(numero, mensaje, date.hour,date.minute)
-def send_image(numero, imagen):
+def send_image(numero: int, imagen: str):
   seconds = time.time() + 60
   date = datetime.fromtimestamp(seconds)
   what.sendwhats_image(numero,imagen)
-  
+
+l = input("quieres mandar mensaje: ")
+if l == "si" or "s":
+  i2 = int(input("a que numero: "))
+  i3 = str(input("mensaje: "))
+  send_message(numero=i2, mensaje=i3)
+if l == "no" or "n":
+  i1 = int(input("a que numero: "))
+  i2 = str(input("dirrecion de imagen: "))
+  send_image(numero=i1, imagen=i2)
